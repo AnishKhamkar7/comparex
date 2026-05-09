8738d7ebefad6396cabcfd052d9e22e93abdff51
AnishKhamkar <117926974+AnishKhamkar7@users.noreply.github.com>
2026-05-09 11:59:56 +0530

    feat: implement user authentication with Prisma and PostgreSQL, update database schema and scripts

--- packages/hono/src/db/index.ts
+++ packages/hono/src/db/index.ts
@@ -1,15 +1,30 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { env } from '../env';
+import { PrismaClient } from '../../src/generated';
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const db =
-  globalThis.prisma ??
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-  });
+function createPrismaClient() {
+  const connectionString = env.DATABASE_URL;
 
-if (process.env.NODE_ENV !== 'production') {
+  if (!connectionString) {
+    throw new Error('DATABASE_URL is missing');
+  }
+
+  return new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString,
+    }),
+
+    log: env.NODE_ENV === 'production' ? ['error'] : ['query', 'error', 'warn'],
+  });
+}
+
+export const db = globalThis.prisma ?? createPrismaClient();
+
+if (env.NODE_ENV !== 'production') {
   globalThis.prisma = db;
 }
